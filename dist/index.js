37d7e19816ad1dc54c7a82d32beccab767b92104
Grant Erickson <grant@granterickson.com>
2021-06-10 22:37:53 -0700

    compile

--- dist/index.js
+++ dist/index.js
@@ -6167,18 +6167,41 @@ var __webpack_exports__ = {};
 const core = __nccwpck_require__(707);
 const github = __nccwpck_require__(621);
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+async function run() {
+    try {
+        console.log(`Starting`);
+
+        // `who-to-greet` input defined in action metadata file
+        const nameToGreet = core.getInput('who-to-greet');
+        console.log(`Hello ${nameToGreet}!`);
+  
+        const time = (new Date()).toTimeString();
+        core.setOutput("time", time);
+  
+        const myToken = core.getInput('myToken');
+        const octokit = github.getOctokit(myToken)
+
+        const data = await octokit.rest.pulls.get({
+            owner: 'octokit',
+            repo: 'rest.js',
+            pull_number: 123,
+            mediaType: {
+                format: 'diff'
+            }
+        });
+        console.log("Data: ");
+        console.log(data);
+    
+        // Get the JSON webhook payload for the event that triggered the workflow
+        const payload = JSON.stringify(github.context.payload, undefined, 2)
+        console.log(`The event payload: ${payload}`);
+
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
+
+run();
 })();
 
 module.exports = __webpack_exports__;
--- index.js
+++ index.js
@@ -1,34 +1,38 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-    console.log(`Starting`);
+async function run() {
+    try {
+        console.log(`Starting`);
 
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
+        // `who-to-greet` input defined in action metadata file
+        const nameToGreet = core.getInput('who-to-greet');
+        console.log(`Hello ${nameToGreet}!`);
   
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
+        const time = (new Date()).toTimeString();
+        core.setOutput("time", time);
   
-    const myToken = core.getInput('myToken');
-    const octokit = github.getOctokit(myToken)
+        const myToken = core.getInput('myToken');
+        const octokit = github.getOctokit(myToken)
 
-    const data = await octokit.rest.pulls.get({
-        owner: 'octokit',
-        repo: 'rest.js',
-        pull_number: 123,
-        mediaType: {
-          format: 'diff'
-        }
-    });
-    console.log("Data: ");
-    console.log(data);
+        const data = await octokit.rest.pulls.get({
+            owner: 'octokit',
+            repo: 'rest.js',
+            pull_number: 123,
+            mediaType: {
+                format: 'diff'
+            }
+        });
+        console.log("Data: ");
+        console.log(data);
     
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+        // Get the JSON webhook payload for the event that triggered the workflow
+        const payload = JSON.stringify(github.context.payload, undefined, 2)
+        console.log(`The event payload: ${payload}`);
 
-} catch (error) {
-    core.setFailed(error.message);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
+
+run();
